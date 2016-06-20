97394bb546b47d123176500c174f6bee58296e57
killme@borg <killme@borg>
2016-06-20 20:16:49 +0200

    testy dla pojedynczej rundy

--- tests.py
+++ tests.py
@@ -12,5 +12,15 @@ class TestRzutyKostka(unittest.TestCase):
         self.assertGreater(game.rzut(), 0)
     #po tych dwoch testach wiemy ze pojedynczy rzut koscia nie daje debilnych wynikow typu -1
 
+class TestRunda(unittest.TestCase):
+
+    def test_rundaMaDacPareWynikow(self):
+        wyn_czlo, wyn_cpu= game.runda()
+        self.assertTrue(wyn_czlo > 1)
+        self.assertTrue(wyn_czlo < 13)
+        self.assertTrue(wyn_cpu > 1)
+        self.assertTrue(wyn_cpu < 13)
+
+
 if __name__ == '__main__':
     unittest.main()
